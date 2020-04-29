d4c6e7d120564616ee728f12c567adb716eda353
eerkela <eerkela42@gmail.com>
2020-04-29 11:20:04 -0700

    Initial Commit

--- [3] Getting and Cleaning Data/Quiz1.R
+++ [3] Getting and Cleaning Data/Quiz1.R
@@ -8,7 +8,6 @@ coursename <- "[3] Getting and Cleaning Data/"
 url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
 filename <- "ACS_2006_microdata_survey_ID.csv"
 
-download.file(as.character(url), paste(coursename, filename, sep=""))
-
-data <- read.csv(paste(coursename, filename, sep=""))
-
+download.file(as.character(url), file.path(coursename, filename))
+data <- read.csv(file.path(coursename, filename))
+sum(data$VAL == 24)
